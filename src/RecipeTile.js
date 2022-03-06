8b93fa5cfc22a80133189fb6f2a2d956a2e90cd7
yumeyuniko <daigocafe@msn.com>
2022-03-06 15:17:46 +0900

    リンク追加

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -6,7 +6,11 @@ export default function RecipeTile({recipe}) {
   return (
     recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/)
     != null && (
-      <div className='recipeTile'>
+      <div className='recipeTile'
+        onClick={() => {
+        window.open(recipe["recipe"]["url"]);
+        }}>
+        
         <img
           className='recipeTile__img'
           src={recipe["recipe"]["image"]}
